e09bbc99297f0f335de4363c62a37cef068f0be7
erobles <erobles@intergrupo.com>
2023-03-22 22:47:40 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,12 @@
+// // la varible nameOfDog = undefined
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito' 
